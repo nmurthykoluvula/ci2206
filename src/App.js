abf5b6bb7bd354247c38ab4cbb34daab0afa0698
Murthy K N <nmurthykoluvula@gmail.com>
2022-09-10 23:58:36 +0530

    added another h1 tag

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This is about Continous Integration - Very Interesting</h1>
+      <h1>Trying to push the code again</h1>
       
     </div>
   );
